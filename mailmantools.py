60002d4bf439fe3f1c5fdc6ba74887cc0ebd1b28
Neil Hawker <neil@jungledog.co.uk>
2017-02-18 20:13:34 +0000

    Added a cached members layer to prevent the need of excessive requests later on when checking if members exist already.

--- mailmantools.py
+++ mailmantools.py
@@ -10,14 +10,22 @@ class MailmanTools:
         self.url = url
         self.listname = listname
         self.__adminpw = adminpw
+        self.__cachedmembers = []
 
-    def getmembers(self):
-        params = self.__getbasequeryparams()
-        page = requests.post(self.membersListUrlFormat.format(self.url, self.listname), headers=self.__headers,
-                             params=params)
-        pagehtml = html.fromstring(page.content)
-        emailaddresses = pagehtml.xpath('//td/a/text()')
-        return emailaddresses
+    def getmembers(self, refresh=False):
+        if refresh or len(self.__cachedmembers) == 0:
+            params = self.__getbasequeryparams()
+            page = requests.post(self.membersListUrlFormat.format(self.url, self.listname), headers=self.__headers,
+                                 params=params)
+            pagehtml = html.fromstring(page.content)
+            members = pagehtml.xpath('//td/a/text()')
+            self.__cachedmembers = members
+            return members
+        else:
+            return self.__cachedmembers
+
+    def getcache(self):
+        return self.__cachedmembers
 
     def __getbasequeryparams(self):
         return {'adminpw': self.__adminpw}
