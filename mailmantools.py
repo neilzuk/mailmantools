8bcf5c14416bbd0f1f018b13458989e724808966
Neil Hawker <neil@jungledog.co.uk>
2017-02-18 20:46:14 +0000

    Changed url formats to constants. Removed getbaseparams method, instead create an object when needed. Renamed memberexists to hasmember. Added addmember and removemember methods which check the cache before carrying out their operations on Mailman.

--- mailmantools.py
+++ mailmantools.py
@@ -3,8 +3,10 @@ import requests
 
 
 class MailmanTools:
-    __headers = {'User-Agent': 'mailmantools/0.1'}
-    membersListUrlFormat = '{0}/mailman/admin/{1}/members/list'
+    __headers = {'User-Agent': 'mailmantools/0.0.1'}
+    MEMBERS_LIST_URL_FORMAT = '{0}/mailman/admin/{1}/members/list'
+    ADD_MEMBERS_URL_FORMAT = '{0}/mailman/admin/{1}/members/add'
+    REMOVE_MEMBERS_URL_FORMAT = '{0}/mailman/admin/{1}/members/remove'
 
     def __init__(self, url, listname, adminpw):
         self.url = url
@@ -14,8 +16,8 @@ class MailmanTools:
 
     def getmembers(self, refresh=False):
         if refresh or len(self.__cachedmembers) == 0:
-            params = self.__getbasequeryparams()
-            page = requests.post(self.membersListUrlFormat.format(self.url, self.listname), headers=self.__headers,
+            params = {'adminpw': self.__adminpw}
+            page = requests.post(self.MEMBERS_LIST_URL_FORMAT.format(self.url, self.listname), headers=self.__headers,
                                  params=params)
             pagetree = html.fromstring(page.content)
             members = pagetree.xpath('//td/a/text()')
@@ -24,8 +26,31 @@ class MailmanTools:
         else:
             return self.__cachedmembers
 
+    def hasmember(self, member):
+        return member in self.__cachedmembers
+
+    def addmember(self, member):
+        if member not in self.__cachedmembers:
+            params = {'adminpw': self.__adminpw, 'subscribe_or_invite': 0, 'send_welcome_msg_to_this_batch': 0,
+                      'send_notifications_to_list_owner': 0, 'subscribees': member}
+            page = requests.post(self.ADD_MEMBERS_URL_FORMAT.format(self.url, self.listname), headers=self.__headers,
+                                 params=params)
+            success = 'Successfully subscribed' in page.text
+            if success:
+                self.__cachedmembers.append(member)
+            return success
+
+    def removemember(self, member):
+        if member in self.__cachedmembers:
+            params = {'adminpw': self.__adminpw, 'send_unsub_ack_to_this_batch': 0, 'send_unsub_notifications_to_list_owner': 0,
+                     'unsubscribees': member}
+            page = requests.post(self.REMOVE_MEMBERS_URL_FORMAT.format(self.url, self.listname),
+                                 headers=self.__headers,
+                                 params=params)
+            success = 'Successfully unsubscribed' in page.text
+            if success:
+                self.__cachedmembers.remove(member)
+            return success
+
     def getcache(self):
         return self.__cachedmembers
-
-    def __getbasequeryparams(self):
-        return {'adminpw': self.__adminpw}
